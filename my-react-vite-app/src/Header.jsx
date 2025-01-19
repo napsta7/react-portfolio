ab55a0523b814cb99d8030f2a5da023b9f694dbc
Paige <paige.renee395@gmail.com>
2025-01-18 23:58:05 -0600

    Installed the Ant Design React component library.

---
+++ my-react-vite-app/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>Paige Harvey Portfolio</h1>
+            <nav>
+                <ul>
+                    <li>About Me</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                    <li>Resume</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+export default Header
